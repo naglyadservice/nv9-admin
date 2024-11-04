ae216fcf4a551ec4557089c2f53f54332834aa98
jane <medovaya.zhenya@gmail.com>
2024-11-04 18:13:06 +0200

    added validation for device

--- watcher.py
+++ watcher.py
@@ -4,7 +4,7 @@ import time
 print("Fiscalization Watcher started");
 
 while True:
-    process = subprocess.Popen("php8.1 artisan app:fiscalize", shell=True)
+    process = subprocess.Popen("/usr/local/phpfarm/inst/php-8.1.21/bin/php artisan app:fiscalize", shell=True)
     process.wait()
 
     time.sleep(1)
